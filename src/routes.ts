4a5384d0d0f12ed7d9002678fe4002960a3378f0
martins20 <pdesignerpb@gmail.com>
2020-08-14 20:39:23 -0300

    Fix Name of files

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,7 @@ const routes = Router();
 
 import PhotoMidleware from './app/middlewares/file';
 
-import FileController from './app/controllers/FIleController';
+import FileController from './app/controllers/FileController';
 
 routes.post('/photos', PhotoMidleware, FileController.store);
 
